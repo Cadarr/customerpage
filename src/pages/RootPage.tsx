d6006aefb509fe0cf5ac204f3ac511c0f859f120
Thorsten Diekhof <kontakt@thorsten-diekhof.de>
2024-03-22 08:27:33 +0100

    feat: api link

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,10 +1,14 @@
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import ApiOutlinedIcon from '@mui/icons-material/ApiOutlined';
 
 const Root = () => {
   const handleGitHubClick = (repositoryName: string) => () => {
     window.open(`https://github.com/Cadarr/${repositoryName}`, "_blank", "noopener,noreferrer");
+  };
+  const handleApiClick = () => () => {
+    window.open(`${import.meta.env.VITE_CUSTOMER_SERVICE_URL}/swagger-ui/index.html`, "_blank", "noopener,noreferrer");
   };
 
   return (
@@ -22,6 +26,10 @@ const Root = () => {
           <Button color="inherit" startIcon={<GitHubIcon />} onClick={handleGitHubClick("customerservice")}>
             Service
           </Button>
+          <Button color="inherit" startIcon={<ApiOutlinedIcon />} onClick={handleApiClick()}>
+            API
+          </Button>
+          
         </Toolbar>
       </AppBar>
 
